88c70317b1773b0a2581e421606446b02ce17eef
Coldyys <92306987+Coldyys@users.noreply.github.com>
2022-06-05 15:40:48 +0200

    Přidejte soubory projektu.

---
+++ GitHub_Brezen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GitHub_Brezen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1 = Console.ReadLine();
+            string n2 = Console.ReadLine();
+            int con1 = Int32.Parse(n1);
+            int con2 = Int32.Parse(n2);
+
+            int min = Math.Min(con1,con2);
+            int max = Math.Max(con1,con2);
+            
+            Console.WriteLine($"{min},{max}");
+
+
+
+
+        }
+    }
+}
