ad19f75a9ef6f6cf8e1f74e02f7217f04e7609b3
Coldyys <92306987+Coldyys@users.noreply.github.com>
2022-06-05 15:47:20 +0200

    Změny textu

--- GitHub_Brezen/Program.cs
+++ GitHub_Brezen/Program.cs
@@ -6,15 +6,19 @@ namespace GitHub_Brezen
     {
         static void Main(string[] args)
         {
+            //Zadání/načtení čísel
+            Console.WriteLine("Zadejte 1. číslo");
             string n1 = Console.ReadLine();
+            Console.WriteLine("Zadejte 2. číslo");
             string n2 = Console.ReadLine();
+            //Konverze z typu string na int
             int con1 = Int32.Parse(n1);
             int con2 = Int32.Parse(n2);
-
+            //Zjistí nejmenší a největší číslo z dvojice
             int min = Math.Min(con1,con2);
             int max = Math.Max(con1,con2);
-            
-            Console.WriteLine($"{min},{max}");
+            //Vypíše finální output
+            Console.WriteLine($"Čísla jsou vypsána od nejmenšího po největší: {min},{max}");
 
 
 
